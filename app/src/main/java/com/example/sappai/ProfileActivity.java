457fa810bd10d4754003756e4869113a57cb8a1e
caiquocdat <71485766+caiquocdat@users.noreply.github.com>
2023-04-21 08:59:55 +0700

    Updating

--- app/src/main/java/com/example/sappai/ProfileActivity.java
+++ app/src/main/java/com/example/sappai/ProfileActivity.java
@@ -27,8 +27,9 @@ import android.widget.RelativeLayout;
 public class ProfileActivity extends AppCompatActivity {
 
     ImageView backImg;
-    LinearLayout languageLinear,voiceLinear;
+    LinearLayout languageLinear, voiceLinear;
     RelativeLayout profileRelative;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +42,84 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        voiceLinear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                View popupView = inflater.inflate(R.layout.popup_voice, null);
+
+                LinearLayout englishLinear = popupView.findViewById(R.id.englishLinear);
+
+                PopupWindow popup = new PopupWindow(
+                        popupView,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+
+
+                RenderScript rs = RenderScript.create(ProfileActivity.this);
+                ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+
+
+                View decorView = getWindow().getDecorView().getRootView();
+                decorView.setDrawingCacheEnabled(true);
+                Bitmap bitmap = decorView.getDrawingCache();
+
+
+                Allocation input = Allocation.createFromBitmap(rs, bitmap);
+
+
+                Allocation output = Allocation.createTyped(rs, input.getType());
+
+                blur.setRadius(25f);
+
+
+                blur.setInput(input);
+                blur.forEach(output);
+
+
+                output.copyTo(bitmap);
+
+                popupView.setBackground(new BitmapDrawable(getResources(), bitmap));
+
+                int margin = (int) getResources().getDimension(R.dimen.popup_margin);
+                popup.setWindowLayoutMode(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                popup.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+                popup.showAtLocation(view, Gravity.CENTER, 0, margin);
+
+                englishLinear.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popup.dismiss();
+                    }
+                });
+
+
+                View activityRootView = getWindow().getDecorView().getRootView();
+
+                activityRootView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        // Check if the touch event occurred outside of the popup window
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            if (!popup.isShowing()) {
+                                return false;
+                            }
+
+                            Rect popupRect = new Rect();
+                            popup.getContentView().getGlobalVisibleRect(popupRect);
+                            if (!popupRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                                // Dismiss the popup window
+                                popup.dismiss();
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                });
+            }
+        });
         languageLinear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,7 +127,7 @@ public class ProfileActivity extends AppCompatActivity {
 
                 View popupView = inflater.inflate(R.layout.popup_language, null);
 
-                LinearLayout englishLinear= popupView.findViewById(R.id.englishLinear);
+                LinearLayout englishLinear = popupView.findViewById(R.id.englishLinear);
 
                 PopupWindow popup = new PopupWindow(
                         popupView,
@@ -85,6 +164,8 @@ public class ProfileActivity extends AppCompatActivity {
                 popupView.setBackground(new BitmapDrawable(getResources(), bitmap));
 
 
+                popup.setWindowLayoutMode(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                popup.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
                 popup.showAtLocation(view, Gravity.CENTER, 0, 0);
 
                 englishLinear.setOnClickListener(new View.OnClickListener() {
@@ -119,17 +200,14 @@ public class ProfileActivity extends AppCompatActivity {
                 });
 
 
-
-
-
             }
         });
     }
 
     private void mapping() {
-        backImg=findViewById(R.id.backImg);
-        languageLinear=findViewById(R.id.languageLinear);
-        voiceLinear=findViewById(R.id.voiceLinear);
-        profileRelative=findViewById(R.id.profileRelative);
+        backImg = findViewById(R.id.backImg);
+        languageLinear = findViewById(R.id.languageLinear);
+        voiceLinear = findViewById(R.id.voiceLinear);
+        profileRelative = findViewById(R.id.profileRelative);
     }
 }
--- app/src/main/java/com/example/sappai/RecentsOpenActivity.java
+++ app/src/main/java/com/example/sappai/RecentsOpenActivity.java
@@ -5,15 +5,22 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sappai.fragment.RecentsFragment;
 
 public class RecentsOpenActivity extends AppCompatActivity {
-    LinearLayout backLinear;
+    LinearLayout backLinear,shareLinear;
+    TextView replyTv;
+    ImageView logoImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +34,22 @@ public class RecentsOpenActivity extends AppCompatActivity {
                 finish();
             }
         });
+        shareLinear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String reply=replyTv.getText().toString();
+                Intent sIntent= new Intent(Intent.ACTION_SEND);
+                sIntent.setType("text/plain");
+                sIntent.putExtra(Intent.EXTRA_TEXT,reply);
+                startActivity(sIntent);
+            }
+        });
     }
 
     private void mapping() {
         backLinear=findViewById(R.id.backLinear);
+        shareLinear=findViewById(R.id.shareLinear);
+        replyTv=findViewById(R.id.replyTv);
+        logoImg=findViewById(R.id.logoImg);
     }
 }
